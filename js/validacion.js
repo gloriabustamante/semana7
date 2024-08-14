7da3dbdb7a30e0042d66b6a3eaa98bfb7cb27a2a
bentancorbelen <bentancorbelen2003@gmail.com>
2024-08-14 18:59:54 -0300

    validacion contraseñas

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,14 @@
+function validarPasswords() {
+    var password1 = document.getElementById("password1").value;
+    var password2 = document.getElementById("password2").value;
+
+    if (password1 != password2) {
+        showAlertError();
+    }
+    else {
+        showAlertSuccess();
+    }
+}
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
